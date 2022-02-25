abd1d4df48ba4f15858ab8e71cb02d798c22ae41
stove-sakaya-teruyoshi <pc-107@st-ove.co.jp>
2022-02-25 13:03:44 +0900

    制作物を追加
    制作物を追加

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function(){
+  // let countFadeOut = 0;
+  // let contents = [$("#header"), $("#main"), $("#sidebar"), $("#footer")];
+  // let progress = [$("#progress-header"), $("#progress-main"), $("#progress-sidebar"), $("#progress-footer")];
+  
+  // function cmp () {
+  //   $(".loading").delay(1000).fadeOut(2000);
+  //   $(".contents").delay(2500).fadeIn(1000);
+  // }
+  
+  // function elmLoaded (elm) {
+  //   elm.fadeOut(1000);
+  //   countFadeOut++;
+  //   if (progress.length >= countFadeOut) {
+  //     cmp();
+  //   }
+  // }
+
+  // $.each(contents, function (index, value) {
+  //   value.on("load", elmLoaded(progress[index]));
+  // });
+
+  let canChange = true;
+  function change (prev, next) {
+    canChange = false;
+    $(".chooser li:nth-child(" + prev + ")").toggleClass("active");
+    $(".chooser li:nth-child(" + next + ")").toggleClass("active");
+    $(".slider li:nth-child(" + prev + ")").fadeOut(1000);
+    $(".slider li:nth-child(" + next + ")").fadeIn(1000);
+    setTimeout(function(){ canChange = true; }, 600);
+  }
+
+  function cmp () {
+    $(".loading").fadeOut(2000);
+    $(".contents").delay(2000).fadeIn(1000);
+  }
+
+  $(window).on("load", cmp());
+
+  let l = 0;
+  $(".chooser li").click(function() {
+    if (canChange) {
+      let n = $(".chooser li").index(this);
+      if (n != l) {
+        change(l+1, n+1);
+        l = n;
+      }
+    }
+  });
+});
